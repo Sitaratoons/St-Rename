b4397cf4dd002447bb395e4f0c651616690c5467
Sitaratoons <mrajayh393798@gmail.com>
2024-12-31 16:25:16 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -11,6 +11,18 @@ from utils import verify_user, check_token
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start(client, message):
+    if not await check_verification(client, message.from_user.id) and VERIFY == True:
+        btn = [[
+            InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{BOT_USERNAME}?start="))
+        ],[
+            InlineKeyboardButton("How To Open Link & Verify", url=VERIFY_TUTORIAL)
+        ]]
+        await message.reply_text(
+            text="<b>You are not verified !\nKindly verify to continue !</b>",
+            protect_content=True,
+            reply_markup=InlineKeyboardMarkup(btn)
+        )
+        return
     user = message.from_user
     await db.add_user(client, message)
     button = InlineKeyboardMarkup([[
